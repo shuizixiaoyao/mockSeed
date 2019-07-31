e30d875e4ba292752c62153a4d885fa1f70834e7
zhenglei <zl_kw@foxmail.com>
2019-07-31 23:29:45 +0800

    修改单元测试

--- src/test/java/com/xxx/xxx/service/PersonServiceTest.java
+++ src/test/java/com/xxx/xxx/service/PersonServiceTest.java
@@ -4,6 +4,7 @@ import com.xxx.xxx.domain.Person;
 import com.xxx.xxx.domain.PersonRequest;
 import com.xxx.xxx.utils.SalaryCalculator;
 import com.xxx.xxx.utils.TimeUnit;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,23 +12,28 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({TimeUnit.class})
+@PrepareForTest({TimeUnit.class, SalaryCalculator.class})
 public class PersonServiceTest {
     @InjectMocks
     private PersonService personService;
+
+    @Before
+    public void setUp() throws Exception {
+        Method sleep = Thread.class.getMethod("sleep", long.class);
+        PowerMockito.replace(sleep).with((proxy, method, args) -> null);
+    }
 
     @Test
     public void should_find_person_when_is_james() {
         //given
         PersonRequest request = new PersonRequest("James");
-        PowerMockito.mockStatic(TimeUnit.class);
-        PowerMockito.doNothing().when(TimeUnit.class);
         //when
         Person person = personService.find(request);
         //then
